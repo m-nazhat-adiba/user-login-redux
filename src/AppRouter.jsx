63278859a3349e26ce163891db777937f0428adb
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-09-17 23:17:21 +0700

    added ProtectedRoute to /

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Users from "./pages/Users";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Users />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Users />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
